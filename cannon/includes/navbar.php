655393c7ad5bc40bd800d771dd100b9c5f5cb7f4
Brett White <brett@pexcil.com>
2018-09-16 18:08:36 +0200

    Updated links within menu

--- cannon/includes/navbar.php
+++ cannon/includes/navbar.php
@@ -1,13 +1,13 @@
 <header id="navbar">
     <div class="container">
         <nav class="menu">
-            <a href="/" class="menu-logo">Cannon Asset Managers</a>
+            <a href="index.php" class="menu-logo">Cannon Asset Managers</a>
             <ul class="menu-links">
-                <li><a href="/" id="menuHome">about</a></li>
-                <li><a href="/investments.php" id="menuInvestments">investments</a></li>
+                <li><a href="index.php" id="menuHome">about</a></li>
+                <li><a href="investments.php" id="menuInvestments">investments</a></li>
                 <li><a href="#!">insights</a></li>
                 <li><a href="#!">news</a></li>
-                <li><a href="/team.php" id="menuTeam">team</a></li>
+                <li><a href="team.php" id="menuTeam">team</a></li>
                 <li><a href="#!">contact</a></li>
             </ul>
             <ul class="menu-buttons">
