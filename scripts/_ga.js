e3ace8701c86e542c291c0a9f96228587ce37a00
Brett White <brett@brettwhite.co.za>
2021-07-08 07:53:20 +0200

    New menu and content
    - Added new nav bar
    - New books
    - New press

---
+++ scripts/_ga.js
@@ -0,0 +1,6 @@
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-120581061-1']);
+_gaq.push(['_setDomainName', 'brettwhite.co.za']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_trackPageview']);
