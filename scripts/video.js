7af60bd0001c162db5c9e3a9ffa5145062c77318
Brett White <brett@brettwhite.co.za>
2024-12-01 19:14:32 -0500

    Video play on page load bug fix

--- scripts/video.js
+++ scripts/video.js
@@ -2,8 +2,8 @@ window.onload = () => {
     document.addEventListener('visibilitychange', () => {
         const video = document.querySelector('video');
         if (document.visibilityState === 'visible' && video) {
-        video.load();
-        video.play();
+            video.load();
+            video.play();
         }
     });
 };
